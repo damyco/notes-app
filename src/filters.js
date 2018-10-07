4b4742f6f193b489a673880dee1758b0e782b7ce
damyco <25842807+damyco@users.noreply.github.com>
2018-10-07 20:11:44 +0100

    first commit

---
+++ src/filters.js
@@ -0,0 +1,21 @@
+const filters = {
+    searchText: 's',
+    sortBy: 'byEdited'
+}
+
+const getFilters = () => filters
+
+const setFilters = (updates) => {
+    if (typeof updates.searchText === 'string') {
+        filters.searchText = updates.searchText
+    }
+    if (typeof updates.sortBy === 'string') {
+        filters.sortBy = updates.sortBy
+    }
+
+}
+
+export {
+    getFilters,
+    setFilters
+}
